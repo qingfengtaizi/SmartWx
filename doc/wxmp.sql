4edb100a2d1da9138440bfd05260d09e1587c7c6
hermit <chenqiang_cool@126.com>
2018-05-29 15:44:13 +0800

    2.0.2 发版

--- doc/wxmp.sql
+++ doc/wxmp.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.24 (32 bit)
 MySQL - 5.7.18 : Database - wxmp
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -14,7 +15,7 @@ MySQL - 5.7.18 : Database - wxmp
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`wxmp` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `wxmp2`;
+USE `wxmp`;
 
 /*Table structure for table `sys_config` */
 
